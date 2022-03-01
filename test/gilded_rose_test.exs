456c42d03f6f4a4ef57b9493499c8e5ede70187f
brandynbennett <brandynlbennett@gmail.com>
2022-03-01 16:08:12 -0700

    alias Item for ease

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -2,9 +2,13 @@ defmodule GildedRoseTest do
   use ExUnit.Case
   doctest GildedRose
 
+  alias GildedRose.Item
+
   test "interface specification" do
     gilded_rose = GildedRose.new()
-    [%GildedRose.Item{} | _] = GildedRose.items(gilded_rose)
+    [%Item{} | _] = GildedRose.items(gilded_rose)
     assert :ok == GildedRose.update_quality(gilded_rose)
   end
+
+  # First test all existing functionality so I don't break anything "even bugs"
 end
