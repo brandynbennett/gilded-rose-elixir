0367c4d5c5cfbb457a3c9598a5edae73a98e78a5
brandynbennett <brandynlbennett@gmail.com>
2022-03-01 16:23:46 -0700

    test sell_in reduction

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -31,4 +31,25 @@ defmodule GildedRoseTest do
     %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20} = backstage
     %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6} = conjured
   end
+
+  test "update_quality sell_in decreases by 1" do
+    gilded_rose = GildedRose.new()
+    :ok = GildedRose.update_quality(gilded_rose)
+
+    [
+      vest,
+      brie,
+      mongoose,
+      sulfuras,
+      backstage,
+      conjured
+    ] = GildedRose.items(gilded_rose)
+
+    %Item{name: "+5 Dexterity Vest", sell_in: 9} = vest
+    %Item{name: "Aged Brie", sell_in: 1} = brie
+    %Item{name: "Elixir of the Mongoose", sell_in: 4} = mongoose
+    %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0} = sulfuras
+    %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 14} = backstage
+    %Item{name: "Conjured Mana Cake", sell_in: 2} = conjured
+  end
 end
