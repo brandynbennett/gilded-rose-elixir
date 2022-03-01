88c0265e0ab81cd14a8891f1c0b07bf173a05b3f
brandynbennett <brandynlbennett@gmail.com>
2022-03-01 16:12:54 -0700

    test initial inventory

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -11,4 +11,24 @@ defmodule GildedRoseTest do
   end
 
   # First test all existing functionality so I don't break anything "even bugs"
+
+  test "initalizes with correct inventory" do
+    gilded_rose = GildedRose.new()
+
+    [
+      vest,
+      brie,
+      mongoose,
+      sulfuras,
+      backstage,
+      conjured
+    ] = GildedRose.items(gilded_rose)
+
+    %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20} = vest
+    %Item{name: "Aged Brie", sell_in: 2, quality: 0} = brie
+    %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7} = mongoose
+    %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80} = sulfuras
+    %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20} = backstage
+    %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6} = conjured
+  end
 end
