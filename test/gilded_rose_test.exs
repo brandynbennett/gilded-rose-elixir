77139da1b04051a2aea0e23f9f491d3dc8799c69
brandynbennett <brandynlbennett@gmail.com>
2022-03-01 16:47:03 -0700

    add all asserts I forgot

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -24,12 +24,15 @@ defmodule GildedRoseTest do
       conjured
     ] = GildedRose.items(gilded_rose)
 
-    %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20} = vest
-    %Item{name: "Aged Brie", sell_in: 2, quality: 0} = brie
-    %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7} = mongoose
-    %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80} = sulfuras
-    %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20} = backstage
-    %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6} = conjured
+    assert %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20} = vest
+    assert %Item{name: "Aged Brie", sell_in: 2, quality: 0} = brie
+    assert %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7} = mongoose
+    assert %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80} = sulfuras
+
+    assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20} =
+             backstage
+
+    assert %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6} = conjured
   end
 
   test "update_quality sell_in decreases by 1 for normal things" do
@@ -45,11 +48,11 @@ defmodule GildedRoseTest do
       conjured
     ] = GildedRose.items(gilded_rose)
 
-    %Item{name: "+5 Dexterity Vest", sell_in: 9} = vest
-    %Item{name: "Aged Brie", sell_in: 1} = brie
-    %Item{name: "Elixir of the Mongoose", sell_in: 4} = mongoose
-    %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 14} = backstage
-    %Item{name: "Conjured Mana Cake", sell_in: 2} = conjured
+    assert %Item{name: "+5 Dexterity Vest", sell_in: 9} = vest
+    assert %Item{name: "Aged Brie", sell_in: 1} = brie
+    assert %Item{name: "Elixir of the Mongoose", sell_in: 4} = mongoose
+    assert %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 14} = backstage
+    assert %Item{name: "Conjured Mana Cake", sell_in: 2} = conjured
   end
 
   test "update_quality quality decreases by 1 for normal things" do
@@ -58,8 +61,8 @@ defmodule GildedRoseTest do
 
     [vest, _, mongoose | _] = GildedRose.items(gilded_rose)
 
-    %Item{name: "+5 Dexterity Vest", quality: 19} = vest
-    %Item{name: "Elixir of the Mongoose", quality: 6} = mongoose
+    assert %Item{name: "+5 Dexterity Vest", quality: 19} = vest
+    assert %Item{name: "Elixir of the Mongoose", quality: 6} = mongoose
   end
 
   test "update_quality quality increases Brie by 1" do
@@ -68,7 +71,7 @@ defmodule GildedRoseTest do
 
     [_, brie | _] = GildedRose.items(gilded_rose)
 
-    %Item{name: "Aged Brie", quality: 1} = brie
+    assert %Item{name: "Aged Brie", quality: 1} = brie
   end
 
   test "update_items can update all items in the inventory" do
