52b01c8c1e7f060123396c74c5ea8ac91c92b168
brandynbennett <brandynlbennett@gmail.com>
2022-03-02 15:08:08 -0700

    Remove old code, add docs

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -3,6 +3,9 @@ defmodule GildedRose do
   alias GildedRose.Item
   alias GildedRose.Inventory
 
+  @doc """
+  Create a new GildedRose
+  """
   def new() do
     inventory =
       Inventory.new([
@@ -19,128 +22,20 @@ defmodule GildedRose do
     agent
   end
 
+  @doc """
+  Get list of all items
+  """
   def items(agent), do: Agent.get(agent, & &1.items)
 
+  @doc """
+  Age inventory 1 day
+  """
   def update_quality(agent) do
     inventory =
       Agent.get(agent, & &1)
       |> Inventory.update_quality()
 
     Agent.update(agent, fn _ -> inventory end)
-
-    # for i <- 0..(Agent.get(agent, &length/1) - 1) do
-    #   item = Agent.get(agent, &Enum.at(&1, i))
-
-    #   item =
-    #     cond do
-    #       item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-    #         if item.quality > 0 do
-    #           if item.name != "Sulfuras, Hand of Ragnaros" do
-    #             %{item | quality: item.quality - 1}
-    #           else
-    #             item
-    #           end
-    #         else
-    #           item
-    #         end
-
-    #       true ->
-    #         cond do
-    #           item.quality < 50 ->
-    #             item = %{item | quality: item.quality + 1}
-
-    #             cond do
-    #               item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-    #                 item =
-    #                   cond do
-    #                     item.sell_in < 11 ->
-    #                       cond do
-    #                         item.quality < 50 ->
-    #                           %{item | quality: item.quality + 1}
-
-    #                         true ->
-    #                           item
-    #                       end
-
-    #                     true ->
-    #                       item
-    #                   end
-
-    #                 cond do
-    #                   item.sell_in < 6 ->
-    #                     cond do
-    #                       item.quality < 50 ->
-    #                         %{item | quality: item.quality + 1}
-
-    #                       true ->
-    #                         item
-    #                     end
-
-    #                   true ->
-    #                     item
-    #                 end
-
-    #               true ->
-    #                 item
-    #             end
-
-    #           true ->
-    #             item
-    #         end
-    #     end
-
-    #   item =
-    #     cond do
-    #       item.name != "Sulfuras, Hand of Ragnaros" ->
-    #         %{item | sell_in: item.sell_in - 1}
-
-    #       true ->
-    #         item
-    #     end
-
-    #   item =
-    #     cond do
-    #       item.sell_in < 0 ->
-    #         cond do
-    #           item.name != "Aged Brie" ->
-    #             cond do
-    #               item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-    #                 cond do
-    #                   item.quality > 0 ->
-    #                     cond do
-    #                       item.name != "Sulfuras, Hand of Ragnaros" ->
-    #                         %{item | quality: item.quality - 1}
-
-    #                       true ->
-    #                         item
-    #                     end
-
-    #                   true ->
-    #                     item
-    #                 end
-
-    #               true ->
-    #                 %{item | quality: item.quality - item.quality}
-    #             end
-
-    #           true ->
-    #             cond do
-    #               item.quality < 50 ->
-    #                 %{item | quality: item.quality + 1}
-
-    #               true ->
-    #                 item
-    #             end
-    #         end
-
-    #       true ->
-    #         item
-    #     end
-
-    #   Agent.update(agent, &List.replace_at(&1, i, item))
-    # end
-
-    # :ok
   end
 
   @doc """
