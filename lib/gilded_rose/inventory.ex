80fbe5d583dbe4d582f004c47797898dbd63373e
brandynbennett <brandynlbennett@gmail.com>
2022-03-02 09:36:14 -0700

    get backstage quality increasing

--- lib/gilded_rose/inventory.ex
+++ lib/gilded_rose/inventory.ex
@@ -4,6 +4,7 @@ defmodule GildedRose.Inventory do
   defstruct items: []
 
   @brie "Aged Brie"
+  @backstage "Backstage passes to a TAFKAL80ETC concert"
 
   def new(items) do
     %__MODULE__{items: items}
@@ -16,6 +17,11 @@ defmodule GildedRose.Inventory do
 
   defp update_items(items) do
     Enum.map(items, &update_item/1)
+  end
+
+  defp update_item(%Item{name: @backstage, sell_in: sell_in, quality: quality} = item) do
+    Map.put(item, :sell_in, sell_in - 1)
+    |> Map.put(:quality, quality + 1)
   end
 
   defp update_item(%Item{name: @brie, sell_in: sell_in, quality: quality} = item) do
