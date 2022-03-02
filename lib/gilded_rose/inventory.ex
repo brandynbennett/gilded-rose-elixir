0356bd60072b8ab34e61808e98a36e52fd7578fc
brandynbennett <brandynlbennett@gmail.com>
2022-03-02 09:21:20 -0700

    add bare bones update_quality function

--- lib/gilded_rose/inventory.ex
+++ lib/gilded_rose/inventory.ex
@@ -4,4 +4,17 @@ defmodule GildedRose.Inventory do
   def new(items) do
     %__MODULE__{items: items}
   end
+
+  def update_quality(%__MODULE__{items: items} = inventory) do
+    items = update_items(items)
+    Map.put(inventory, :items, items)
+  end
+
+  defp update_items(items) do
+    Enum.map(items, &update_item/1)
+  end
+
+  defp update_item(item) do
+    item
+  end
 end
